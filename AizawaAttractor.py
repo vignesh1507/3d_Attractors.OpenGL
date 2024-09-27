f8c00e83ff349713fabe2d618e4f1672305b7394
Vignesh Skanda <agvskanda@gmail.com>
2024-09-27 20:43:16 +0530

    Update AizawaAttractor.py

--- AizawaAttractor.py
+++ AizawaAttractor.py
@@ -24,7 +24,7 @@ class Simulation(object):   #create a class with all the following data:
 
     #run algorithm and draw lines
     def Update(self):
-        #here starts the algoritm, it runs 5000 times
+        #here starts the algorithm, it runs 5000 times
         count = 0
         for i in range(0, 1000000, 1): 
             dx = (((self.z - self.b) * self.x) - (self.d * self.y)) * self.deltatime
