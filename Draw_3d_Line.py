859470057341d766b5629ed964b79d822bb0c76b
Vignesh Skanda <agvskanda@gmail.com>
2024-09-27 20:40:47 +0530

    Update Draw_3d_Line.py
    Updated some changes in the code that are as follows:
    
    1. Deprecation of QtGui: In PyQt5 and PyQt6, QApplication has been moved to QtWidgets, so you should import and use QtWidgets.QApplication instead of QtGui.QApplication.
    
    2. Global variable points_list: It's unnecessary to use a global variable inside a class. You can replace global points_list with an instance variable (self.points_list) to store the points.

--- Draw_3d_Line.py
+++ Draw_3d_Line.py
@@ -1,35 +1,33 @@
-import numpy as np 
+import numpy as np
 import pyqtgraph as pg
-from pyqtgraph.Qt import *
+from pyqtgraph.Qt import QtWidgets
 import pyqtgraph.opengl as gl
 import sys
 
 
 class Simulation():
     def __init__(self):
-        self.app = QtGui.QApplication(sys.argv)
+        self.app = QtWidgets.QApplication(sys.argv)
         self.window = gl.GLViewWidget()     
-        self.window.setGeometry(100, 100, 800, 600) #set the geometry of the window(x padding, y padding, dimension x, dimension y)
-        self.window.setWindowTitle("I am going to draw a line") #set the title
-        self.window.show()  #show the window
+        self.window.setGeometry(100, 100, 800, 600) 
+        self.window.setWindowTitle("I am going to draw a line") 
+        self.window.show()
 
-        
-        global points_list
-        points_list = [] #create an empty list
-        self.draw() #call the draw function
+        self.points_list = []  # Use an instance variable instead of global
+        self.draw()
 
     def draw(self):
-        point1 = (0, 0, 0)  #specify the (x, y, z) values of the first point in a tuple
-        point2 = (5, 6, 8)  #specify the (x, y, z) values of the second point in a tuple
+        point1 = (0, 0, 0)  
+        point2 = (5, 6, 8)  
 
-        points_list.append(point1) #add the point1 tuple to the points_list
-        points_list.append(point2) #add the point2 tuple to the points_list
-        print(points_list)
-        points_array = np.array(points_list) #convert the list to an array
-        drawing_variable = gl.GLLinePlotItem(pos = points_array, width = 1, antialias = True)   #make a variable to store drawing data(specify the points, set antialiasing)
-        self.window.addItem(drawing_variable) #draw the item
+        self.points_list.append(point1)
+        self.points_list.append(point2)
+        print(self.points_list)
+        points_array = np.array(self.points_list) 
+        drawing_variable = gl.GLLinePlotItem(pos=points_array, width=1, antialias=True)
+        self.window.addItem(drawing_variable)
 
     def start(self):
-        QtGui.QApplication.instance().exec_() #run the window properly
+        QtWidgets.QApplication.instance().exec_()  # Run the window properly
 
 Simulation().start()
